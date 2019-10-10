6f227317fc4c4e98f0f81c655ae9d781b6334760
Kathryn DiPippo <kathryn.dipippo@acquia.com>
2019-10-10 10:06:47 -0400

    Fixed manage.html styling

--- scripts/export.js
+++ scripts/export.js
@@ -1,26 +1,38 @@
 // exports opml -> bookmarks
 // TODO - write comment
 function ExportBookmarks(startNode) {
-    chrome.bookmarks.getChildren(startNode[0].id, function(nodes) {
-        var opml = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?><opml version=\"2.0\">\n<head><title>Slick RSS OPML Export</title></head>\n<body>";
-        for (var i = 0; i < nodes.length;i++) {
-            opml += "<outline type=\"rss\" text=\"" + nodes[i].title.replace("&", "&amp;") + "\" xmlUrl=\"" + nodes[i].url.replace("&", "&amp;") + "\"/>\n";
-        }
-        opml += "</body>\n</opml>";
-        document.getElementById("opml").value = opml;
-    });
+  chrome.bookmarks.getChildren(startNode[0].id, function(nodes) {
+    var opml = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?><opml version=\"2.0\">\n<head><title>Slick RSS OPML Export</title></head>\n<body>";
+    let opmlcontent = "";
+    for (var i = 0; i < nodes.length; i++) {
+      let opmlText = nodes[i].title.replace("&", "&amp;");
+      let opmlXmlUrl= nodes[i].url.replace("&", "&amp;");
+      opmlcontent += `<outline type="rss" text="${opmlText}" xmlUrl="${opmlXmlUrl}">`;
+    }
+    let opml = `<?xml version="1.0" encoding="ISO-8859-1"?><opml version="2.0">
+      <head><title>Slick RSS OPML Export</title></head>
+      <body>${opmlcontent}</body>
+    </opml>`;
+    document.getElementById("opml").value = opml;
+  });
 }
 
+// TODO - refactor so both ExportBookmarks and ExportFeeds are calling the same function to generate the opml
+//   right now the code is repeated but nodes.length is different from bgPage.feeds.length
 // imports opml -> feed list
 // TODO - write comment
 function ExportFeeds() {
-    var opml = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?><opml version=\"2.0\">\n<head><title>Slick RSS OPML Export</title></head>\n<body>";
-    for (var i = 0; i < bgPage.feeds.length;i++) {
-        if (bgPage.feeds[i].title != "Read Later") {
-            opml += "<outline type=\"rss\" text=\"" + bgPage.feeds[i].title.replace("&", "&amp;") + "\" xmlUrl=\"" + bgPage.feeds[i].url.replace("&", "&amp;") + "\"/>\n";
-        }
+  let opmlcontent = "";
+  for (var i = 0; i < bgPage.feeds.length; i++) {
+    if (bgPage.feeds[i].title != "Read Later") {
+      let opmlText = bgPage.feeds[i].title.replace("&", "&amp;");
+      let opmlXmlUrl= bgPage.feeds[i].url.replace("&", "&amp;");
+      opmlcontent += `<outline type="rss" text="${opmlText}" xmlUrl="${opmlXmlUrl}">`;
     }
-    opml += "</body>\n</opml>";
-
-    document.getElementById("opml").innerText = opml;
+  }
+  let opml = `<?xml version="1.0" encoding="ISO-8859-1"?><opml version="2.0">
+      <head><title>Slick RSS OPML Export</title></head>
+      <body>${opmlcontent}</body>
+    </opml>`;
+  document.getElementById("opml").innerText = opml;
 }
--- scripts/export_render.js
+++ scripts/export_render.js
@@ -1,9 +1,9 @@
 var bgPage = chrome.extension.getBackgroundPage();
 
 $(document).ready(function() {
-    if (bgPage.options.feedsource == 1) {
-        chrome.bookmarks.get(bgPage.options.feedfolderid, ExportBookmarks);
-    } else {
-        ExportFeeds();
-    }
+  if (bgPage.options.feedsource == 1) {
+    chrome.bookmarks.get(bgPage.options.feedfolderid, ExportBookmarks);
+  } else {
+    ExportFeeds();
+  }
 });
